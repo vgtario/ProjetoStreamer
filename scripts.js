8700bd53dbb5a037600ef0c1d85b9dd0e34829c2
vgtario <139800025+vgtario@users.noreply.github.com>
2023-07-24 14:09:51 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+function abrirNav() {
+    document.getElementById("Menuoculto").style.width = "250px";
+    //document.getElementById("principal").style.marginLeft = "250px";
+}
+
+function fecharNav() {
+    document.getElementById("Menuoculto").style.width = "0";
+   // document.getElementById("principal").style.marginLeft = "0";
+}
+
+
+
+
+
